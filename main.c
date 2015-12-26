10880a4bcdafba25b8065019e8799cabad22e34c
Nur Hussein <hussein@unixcat.org>
2015-12-27 03:03:10 +0800

    Made the welcome message fancier.

--- main.c
+++ main.c
@@ -11,5 +11,16 @@ int kernel_main() {
 	print_char('l', 40, 12, COLOR_BROWN, COLOR_BLACK);
 	print_char('o', 41, 12, COLOR_LIGHT_GREEN, COLOR_BLACK);
 
+	print_char('T', 35, 13, COLOR_BROWN, COLOR_BLACK);
+	print_char('r', 36, 13, COLOR_LIGHT_GREEN, COLOR_BLACK);
+	print_char('e', 37, 13, COLOR_LIGHT_CYAN, COLOR_BLACK);
+	print_char('e', 38, 13, COLOR_GREEN, COLOR_BLACK);
+	print_char('h', 39, 13, COLOR_BROWN, COLOR_BLACK);
+	print_char('o', 40, 13, COLOR_LIGHT_GREEN, COLOR_BLACK);
+	print_char('u', 41, 13, COLOR_BROWN, COLOR_BLACK);
+	print_char('s', 42, 13, COLOR_LIGHT_GREEN, COLOR_BLACK);
+	print_char('e', 43, 13, COLOR_BROWN, COLOR_BLACK);
+
+
 	return 0xDEADBEEF;
 }
